e78e4fa5446fbee7dc795a8f31e25873df0d3d5a
Isara Madunika <99160274+isaramadunika@users.noreply.github.com>
2025-01-23 22:18:23 +0530

    Adde the new char

--- da.R
+++ da.R
@@ -4,3 +4,8 @@ library(palmerpenguins)
 penguins %>%
   ggplot(aes(x=bill_depth_mm))+
   geom_histogram()
+# added the anothe chart
+
+penguins %>%
+  ggplot(aes(x=bill_length_mm))+
+  geom_boxplot()
