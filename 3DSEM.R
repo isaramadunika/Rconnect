0f93e28ec1382a66434a5518beb8eb9a81b3c9eb
Isara Madunika <99160274+isaramadunika@users.noreply.github.com>
2025-01-24 00:43:01 +0530

    Show the data set head, That is help for select matched dataset.

---
+++ 3DSEM.R
@@ -0,0 +1,9 @@
+
+
+data1 <- read.csv("C:\\Users\\shard\\Desktop\\R_Studio_Connect\\TeleCom_2009_2023_Daily.csv")
+data2 <- read.csv("C:\\Users\\shard\\Desktop\\R_Studio_Connect\\TeleCom_2009_2023_Monthly.csv")
+data3 <- read.csv("C:\\Users\\shard\\Desktop\\R_Studio_Connect\\TeleCom_2009_2023_Weekly.csv")
+
+head(data1)
+head(data2)
+head(data3)
