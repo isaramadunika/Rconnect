4a0f8bf1a170ff47394acc2416503441b8b3ac89
Isara Madunika <99160274+isaramadunika@users.noreply.github.com>
2025-01-24 13:47:46 +0530

    Fill the missing value

--- 3DSEM.R
+++ 3DSEM.R
@@ -25,6 +25,10 @@ df$Date <- as.Date(df$Date, format = "%m/%d/%Y")
 # Check for missing values
 colSums(is.na(df))  # Count of missing values in each column
 
+# Fill missing values (if any)
+df <- df %>% 
+  mutate(across(where(is.numeric), ~ ifelse(is.na(.), zoo::na.approx(.), .)))  # Interpolate missing numeric values
+
 # Apply to numerical columns
 data_daily <- data_daily %>% mutate_if(is.numeric, remove_outliers)
 data_daily <- data_daily %>% mutate_all(~ifelse(is.na(.), zoo::na.approx(.), .))
