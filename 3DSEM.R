06e66e4a27492e3d7b4d36da67022c28b6dfff1e
Isara Madunika <99160274+isaramadunika@users.noreply.github.com>
2025-01-24 13:35:40 +0530

    Identify the main vs data set in dataset

--- 3DSEM.R
+++ 3DSEM.R
@@ -5,12 +5,20 @@ library(forecast)
 library(caret)
 library(prophet)
 
-data1 <- read.csv("C:\\Users\\shard\\Desktop\\R_Studio_Connect\\TeleCom_2009_2023_Daily.csv")
-data2 <- read.csv("C:\\Users\\shard\\Desktop\\R_Studio_Connect\\TeleCom_2009_2023_Monthly.csv")
-data3 <- read.csv("C:\\Users\\shard\\Desktop\\R_Studio_Connect\\TeleCom_2009_2023_Weekly.csv")
+# Read the datasets
+data_daily <- read.csv("C:\\Users\\shard\\Desktop\\R_Studio_Connect\\TeleCom_2009_2023_Daily.csv")
+data_monthly <- read.csv("C:\\Users\\shard\\Desktop\\R_Studio_Connect\\TeleCom_2009_2023_Monthly.csv")
+data_weekly <- read.csv("C:\\Users\\shard\\Desktop\\R_Studio_Connect\\TeleCom_2009_2023_Weekly.csv")
 
-head(data1)
-head(data2)
-head(data3)
+# Inspect structure and summary
+str(data_daily)
+summary(data_daily)
+
+# Check for missing values
+colSums(is.na(data_daily))
+colSums(is.na(data_monthly))
+colSums(is.na(data_weekly))
+
+
 
 
